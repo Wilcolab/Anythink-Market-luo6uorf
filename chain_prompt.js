34c00f85345c7e10637dc1ba17645debf40968f4
WilcoApp <hello@wilco.gg>
2025-07-08 07:29:37 +0000

    Initial commit

--- chain_prompt.js
+++
@@ -1,5 +0,0 @@
-function toKebabCase(str) {
-    return str
-        .replace(/[\s_]+/g, '-')   // Replace spaces and underscores with hyphens
-        .toLowerCase();            // Convert to lowercase
-}
